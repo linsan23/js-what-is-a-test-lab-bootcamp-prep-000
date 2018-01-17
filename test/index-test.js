92220ba582c654044be3d629fecf159ed390b671
leensan chong <linsan2304@gmail.com>
2018-01-17 18:53:16 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -15,7 +15,7 @@ describe('what-is-a-test', () => {
   })
 
   describe('Message', () => {
-    it('gives the name and height' to include 'Susan', () => {
+    it('gives the name and height',() => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
